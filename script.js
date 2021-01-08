74760de46b7b52d6cb80ad00291e6acd22db9364
Kevin Walorz <kmwalorz@gmail.com>
2021-01-08 18:30:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let button of btnsOpenModal) {
+  console.log(button.textContent);
+}
